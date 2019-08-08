34d1b42816d9e380d9776a1b7ddd8e6af7ee38ff
oliviaalchemy <olivia@alchemytechs.com>
2019-08-08 14:19:57 +0100

    Add project files.

---
+++ Getters and Setters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Getters_and_Setters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movies avengers = new Movies("The Avengers", "Joss Whedon", "PG-13");
+            Movies shrek = new Movies("Shrek", "Adam Admson", "PG");
+            //G, PG, PG-13, R, NR
+            
+            Console.WriteLine(avengers.Rating);
+            Console.ReadLine();
+        }
+    }
+}
